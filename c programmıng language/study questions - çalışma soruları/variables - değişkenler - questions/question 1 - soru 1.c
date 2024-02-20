a2ea5681611cab5bf095399f34d7a817938a4c10
Umar Shahzad <160236889+umarshahzad-dev@users.noreply.github.com>
2024-02-20 16:40:49 +0300

    Update question 1 - soru 1.c

--- c programmıng language/study questions - çalışma soruları/variables - değişkenler - questions/question 1 - soru 1.c
+++ c programmıng language/study questions - çalışma soruları/variables - değişkenler - questions/question 1 - soru 1.c
@@ -1,1 +1,13 @@
+//y= 3*x^2 You need to code the C program for the function. Example: If x=2, y=12. 
+//y= 3*x^2 Fonksiyona ait C programını kodlamanız gerekir. Örnek: x=2 ise y=12 olmalıdır.
 
+#include<stdio.h>
+int main()
+{
+    float x , y ;
+    printf("Enter the value of x : ");
+    scanf("%f",&x);
+    y = 3*x*x;
+    printf("\nThe value of y when x=%.2f is %.2f ",x,y);
+    return 0;
+}
